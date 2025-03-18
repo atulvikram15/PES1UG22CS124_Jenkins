b5fde75977cbd9332b9355e824bf578cf1c38081
atulvikram15 <atulvikram04@gmail.com>
2025-03-18 19:13:09 +0530

    Updated hello.cpp with a new changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hi,CC\n";
+  cout << "good day\n";
   cout << "Hello, TAs, thank you for annoying me like this\n";
   cout << "I have successfully built and run this  \n";
   return 0;
