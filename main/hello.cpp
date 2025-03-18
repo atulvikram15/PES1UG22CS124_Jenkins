33b2a138c34e1e56696587a44147556838f3313a
atulvikram15 <atulvikram04@gmail.com>
2025-03-18 19:08:15 +0530

    Updated hello.cpp with better changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "I am Atul\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hi,CC\n";
+  cout << "Hello, TAs, thank you for annoying me like this\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
